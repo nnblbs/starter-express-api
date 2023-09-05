67d6a69537695a5223251309f6a4a72af5a4a223
nnblbs <143826040+nnblbs@users.noreply.github.com>
2023-09-05 06:05:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+app.post('/SyNZrzEAEgJepbkzVnHtUwyPFbWCEJXA', (req, res) => {
+  res.send('tg')
 })
+
+app.post('/pBxgAeSeViGnFJGFqhvMgTsPOWcHwIsh', (req, res) => {
+  res.send('eth')
+})
+
 app.listen(process.env.PORT || 3000)
